a85000a57b0e7389c410197c1ff364c0dfead4ee
gawela <abel.gawel@mavt.ethz.ch>
2017-03-23 17:29:44 +0100

    Split into x-view_node and x-view_core.

--- x-view_ros/x-view_node/include/x-view_node/x-view_node.h
+++ x-view_ros/x-view_node/include/x-view_node/x-view_node.h
@@ -1,5 +1,5 @@
 #ifndef X_VIEW_NODE_H_
-#define X_VIEW_NODE_
+#define X_VIEW_NODE_H_
 
 #include <gtsam/nonlinear/NonlinearFactorGraph.h>
 #include <kindr/minimal/quat-transformation.h>
@@ -9,39 +9,12 @@
 
 // TODO(gawela): Move non-ros functions to non ros package.
 namespace x_view_ros {
-typedef kindr::minimal::QuatTransformationTemplate<double> SE3;
-
-struct XViewParams { }; // struct XViewParams
-
-struct XViewSemantics {
-  SE3 pose;
-  gtsam::NonlinearFactorGraph& factor_graph;
-}; // struct XViewSemantics
 
 class XViewNode {
 
  public:
   explicit XViewNode(ros::NodeHandle& n);
   ~XViewNode();
-
-  /// \brief Extract semantic descriptor from semantics image.
-  void extractSemanticsFromImage(const cv::Mat& image, const SE3& pose,
-                                 XViewSemantics* semantics_out);
-
-  /// \brief Match semantics instance to database and return score.
-  void matchSemantics(const XViewSemantics& semantics_a,
-                      Eigen::MatrixXd* matches);
-
-  /// \brief Filter matches, e.g., geometric verification etc.
-  void filterMatches(const XViewSemantics& semantics_a,
-                     Eigen::MatrixXd* matches);
-
-  /// \brief Merge semantics instance into database according to matches.
-  void mergeSemantics(const XViewSemantics& semantics_a,
-                      const Eigen::MatrixXd& matches);
-
-  /// \brief Clean database by doing full semantics matching.
-  void cleanDatabase();
 
  private:
   // Get ROS parameters.
@@ -52,9 +25,6 @@ class XViewNode {
   // Node handle.
   ros::NodeHandle& nh_;
 
-  // Parameters.
-  XViewParams params_;
-
   // Subscribers.
   ros::Subscriber image_sub_;
 
@@ -64,8 +34,7 @@ class XViewNode {
   // Services.
   ros::ServiceServer trigger_xview_;
 
-  std::vector<XViewSemantics> semantics_db_;
 }; // XViewNode
 
 }
-#endif /* X_VIEW_NODE_ */
+#endif /* X_VIEW_NODE_H_ */
