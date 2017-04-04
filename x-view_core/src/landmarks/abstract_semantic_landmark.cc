5913814897da41134363b434c196e5c3087eb017
gawela <abel.gawel@mavt.ethz.ch>
2017-04-04 16:50:04 +0200

    Deleted redundant files.

--- x-view_core/src/landmarks/abstract_semantic_landmark.cc
+++
@@ -1,8 +0,0 @@
-#include <x-view_core/landmarks/abstract_semantic_landmark.h>
-
-namespace x_view {
-
-    AbstractSemanticLandmark::AbstractSemanticLandmark(const cv::Mat &image, const SE3 &pose)
-            : image_(image), pose_(pose) {}
-
-}
