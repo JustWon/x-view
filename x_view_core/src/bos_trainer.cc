e6b8fb0d3c9cc8309ca8f8028136feed5c5c8b33
Carlo Del Don <carlo.deldon@gmail.com>
2017-04-05 10:08:32 +0200

    Started implementing a Bag of Semantics trainer

---
+++ x_view_core/src/bos_trainer.cc
@@ -0,0 +1,25 @@
+#include <x_view_core/bos_trainer.h>
+#include <x_view_core/bos.h>
+
+namespace x_view {
+
+BoSTrainer::BoSTrainer(int clusterCount,
+                       const cv::TermCriteria& termcrit,
+                       int attempts,
+                       int flags)
+    : cv::BOWKMeansTrainer(clusterCount, termcrit, attempts, flags) {
+
+}
+
+BoSTrainer::~BoSTrainer() {
+
+}
+
+void BoSTrainer::add(const SemanticLandmarkPtr& semanticLandmarkPtr) {
+  std::shared_ptr<BoS> bos = std::dynamic_pointer_cast<BoS>(semanticLandmarkPtr);
+  CHECK(bos != nullptr) << "semanticLandmarkPtr passed to " << __FUNCTION__
+                        << "might not be an instance of 'BoS' class";
+}
+
+}
+
