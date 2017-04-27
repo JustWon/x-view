a297dcd915733f77ccd4e768415c6b43af4ef8b2
Carlo Del Don <carlo.deldon@gmail.com>
2017-04-27 11:05:45 +0200

    moved test implementation to new file to keep test_graph_landmark.cc clean

---
+++ x_view_core/test/test_graph_landmark_impl.h
@@ -0,0 +1,108 @@
+#ifndef X_VIEW_TEST_GRAPH_LANDMARK_IMPL_H
+#define X_VIEW_TEST_GRAPH_LANDMARK_IMPL_H
+
+#include <gtest/gtest.h>
+
+#include <x_view_core/x_view_types.h>
+#include <x_view_core/datasets/abstract_dataset.h>
+#include <x_view_core/features/graph_descriptor.h>
+#include <x_view_core/landmarks/graph_landmark.h>
+
+#include <opencv2/core/core.hpp>
+
+#include <boost/progress.hpp>
+
+using namespace x_view;
+
+typedef std::shared_ptr<GraphLandmark> GraphLandmarkPtr;
+
+/// \brief test the custom image
+void testCustomImage();
+/// \brief test the chessboard image
+void testChessboardImage();
+/// \brief test the disc image
+void testDiscImage();
+
+/**
+ * \brief Counts how many pixels with each label are present in the image
+ * \param image cv::Mat to be analyzed
+ * \param pixelCount vector filled up with the pixel count such that
+ * 'pixelCount[i]' contains the number of pixels in 'image' having label 'i'
+ */
+void countPixelLabelsInImage(const cv::Mat& image,
+                             std::vector<int>& pixelCount);
+
+/**
+ * \brief Checks if the number of pixels for each class 'i' in 'image' is the
+ * same between counting them explicitly and the one contained in the
+ * graphLandmarkPointer
+ * \param graphLandmarkPtr pointer to the graphLandmark
+ * \param imageName logging image name
+ */
+void testPixelCount(const GraphLandmarkPtr& graphLandmarkPtr,
+                    const std::string& imageName);
+
+/**
+ * \brief Checks if the number of instances per class found by the
+ * graphLandmarkPrt object is the same as the expected one
+ * \param graphLandmarkPtr pointer to the graphLandmark
+ * \param expectedInstanceCount expected number of instances per class, such
+ * that 'expectedInstanceCount[i]' contains the number of expected instances
+ * for class 'i'
+ * \param imageName logging image name
+ */
+void testInstanceCount(const GraphLandmarkPtr& graphLandmarkPtr,
+                       const std::vector<int>& expectedInstanceCount,
+                       const std::string& imageName);
+
+/**
+ * \brief Checks that no pixel appears twice in the blob structure generated
+ * by the graphLandmark pointed by the passed parameter
+ * \param graphLadmarkPtr pointer to the graphLandmark
+ * \param imageName logging image name
+ */
+void testDuplicatePixels(const GraphLandmarkPtr& graphLadmarkPtr,
+                         const std::string& imageName);
+
+/**
+ * \brief Creates a custom image of size 'desiredRows' x 'desiredCols'
+ * \param desiredRows desired number of rows
+ * \param desiredCols desired number of cols
+ * \param image generated image
+ */
+void createCustomImage(const int desiredRows, const int desiredCols,
+                       cv::Mat& image);
+
+/**
+ * \brief Creates a chessboard-like image of approximate size 'desiredRows' x
+ * 'desiredCols' with a block size of 'block_size'. The generated image
+ * represents a sets of blocks in the following order:
+ * 0, 1, 2, 3, ... , N-1, 0, 1, 2, 3, ..., N-1, ...., N-1
+ * 1, 2, 3, 4, ... , 0, 1, 2, 3, 4, ...
+ * ...
+ * where 'N' is equal to 'globalDatasetPtr->numSemanticClasses()'.
+ * For this reason the final image size might not be exactly the same as the
+ * one passed as parameter
+ * \param desiredRows desired number of rows
+ * \param desiredCols desired number of cols
+ * \param block_size size of the chessboard block
+ * \param image generated image
+ */
+void createChessBoardImage(const int desiredRows, const int desiredCols,
+                           const int block_size, cv::Mat& image);
+
+/**
+ * \brief Creates an image containing a set of discs
+ * \param desiredRows desired number of rows
+ * \param desiredCols desired number of cols
+ * \param centers vector of disc centers (cv::Point)
+ * \param radii vector of disc radii
+ * \param labels vector of label to associate to each disc
+ * \param image generated image
+ */
+void createDiscImage(const int desiredRows, const int desiredCols,
+                     const std::vector<cv::Point>& centers,
+                     const std::vector<int> radii,
+                     const std::vector<int> labels, cv::Mat& image);
+
+#endif //X_VIEW_TEST_GRAPH_LANDMARK_IMPL_H
