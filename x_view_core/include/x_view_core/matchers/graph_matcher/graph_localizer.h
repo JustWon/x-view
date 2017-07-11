02a0d8a5bde550a6e446989faae63afb81434b62
Carlo Del Don <carlo.deldon@gmail.com>
2017-07-11 11:15:54 +0200

    documentation

--- x_view_core/include/x_view_core/matchers/graph_matcher/graph_localizer.h
+++ x_view_core/include/x_view_core/matchers/graph_matcher/graph_localizer.h
@@ -36,8 +36,16 @@ class GraphLocalizer {
 
  private:
   struct Observation {
+    /// \brief Vertex property associated to the observation. Its 3D location
+    /// is used for localization.
     VertexProperty vertex_property;
+    /// \brief Observed distance from the camera to the semantic entity
+    /// associated with the vertex property.
     double distance;
+    /// \brief Evidence associated to the observation. This value could be
+    /// associated to the vertex similarity computed during graph matching
+    /// and is used in the localization procedure for the computation of the
+    /// noise model associated to the observation factors.
     double evidence;
   };
 
