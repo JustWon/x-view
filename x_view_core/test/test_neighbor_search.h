21194457f3402416014e2fdd028749b3c1c2b715
Carlo Del Don <carlo.deldon@gmail.com>
2017-08-14 11:03:35 +0200

    documentation on test header

--- x_view_core/test/test_neighbor_search.h
+++ x_view_core/test/test_neighbor_search.h
@@ -15,17 +15,45 @@ typedef Eigen::Matrix<x_view::real_t, 2, 1> Vector2r;
 
 typedef Nabo::NearestNeighbourSearch<x_view::real_t> NNSearch;
 
+/**
+ * \brief Tests the functionality of libnabo by checking if the computed
+ * nearest neighbors correspond to a set of manually selected ones.
+ */
 void testNabo2D();
 
+/**
+ * \brief Generates N D-dimensional points on a 1-dimensional line aligned
+ * with the first coordinate between lower_bound and upper_bound.
+ * \param N Numbers of points to generate.
+ * \param D Dimensionality of the points being generated.
+ * \param lower_bound Scalar indicating the smallest value associated to the
+ * first coordinate.
+ * \param upper_bound Scalar indicating the largest value associated to the
+ * first coordinate.
+ * \param points Matrix of size DxN filled up with the generated points.
+ */
 void generatePointsOnLine(const uint64_t N, const uint64_t D,
                           const x_view::real_t lower_bound,
                           const x_view::real_t upper_bound,
                           x_view::MatrixXr* points);
 
+/**
+ * \brief Tests the results provided by libnabo.
+ * \param points Pointcloud over which libnabo builds a KD-tree.
+ * \param query Query point for which libnabo searches nearest neighbors in
+ * the pointcloud.
+ * \param expected_indices Ordered vector of indices in pointcloud specifying
+ * closest neighbors of query point.
+ */
 void testKNN(const x_view::MatrixXr& points, const Vector2r& query,
              const std::vector<int>& expected_indices);
 
 
+/**
+ * \brief Builds two x_view::Blob instances with random external_pixels and
+ * tests the perdormance of the x_view::Blob::areNeighors() function for
+ * using the functionalities offered by libnabo and the bruteforce approach.
+ */
 void testBlobNeighborPerformance();
 
 }
