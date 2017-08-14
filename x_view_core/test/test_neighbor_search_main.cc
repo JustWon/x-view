99ce403cf1d2a17103e2932e17effbdc7bf775c8
Carlo Del Don <carlo.deldon@gmail.com>
2017-08-14 11:05:24 +0200

    added minor comments

--- x_view_core/test/test_neighbor_search_main.cc
+++ x_view_core/test/test_neighbor_search_main.cc
@@ -9,8 +9,11 @@ TEST(XViewSlamTestSuite, test_neighbor_search) {
   // Tests the neighbor search procedure used in X-View.
   LOG(INFO) << "\n\n====Testing neighbor search====";
 
+  // Test if libnabo works as expected.
   testNabo2D();
 
+  // Test if introducing libnabo and the associated datastructures offers an
+  // advantage over the bruteforce neighbor computation.
   testBlobNeighborPerformance();
 }
 
