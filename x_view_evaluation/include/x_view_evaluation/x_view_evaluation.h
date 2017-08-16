5801884b01c0e16aa8ab498f431caa851bb57e7a
Carlo Del Don <carlo.deldon@gmail.com>
2017-08-16 09:58:27 +0200

    started implementing evaluation package

---
+++ x_view_evaluation/include/x_view_evaluation/x_view_evaluation.h
@@ -0,0 +1,8 @@
+#ifndef X_VIEW_EVALUATION_H
+#define X_VIEW_EVALUATION_H
+
+namespace x_view_evaluation {
+
+}
+
+#endif //X_VIEW_EVALUATION_H
---
+++ x_view_evaluation/src/x_view_evaluation.cc
@@ -0,0 +1,6 @@
+#include <x_view_evaluation/x_view_evaluation.h>
+
+namespace x_view_evaluation {
+
+}
+
