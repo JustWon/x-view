204e9881f9f5cf93f9cb8aff2174a4183cbfd20e
Carlo Del Don <carlo.deldon@gmail.com>
2017-08-17 13:12:05 +0200

    time_printer doc

--- x_view_core/include/x_view_core/timer/timer_printer.h
+++ x_view_core/include/x_view_core/timer/timer_printer.h
@@ -5,23 +5,36 @@
 
 namespace x_view {
 
+/**
+ * \brief Class implementing methods to print a timer in a human readable
+ * format.
+ */
 class TimerPrinter {
 
  public:
   /**
- * \brief Generates a human readable table with information about the
- * registered timers.
- * \return A string containing the generated table.
- */
+   * \brief Generates a human readable table with information about the
+   * registered timers.
+   * \param timer Timer to be transformed into a table string.
+   * \return A string containing the generated table.
+   */
   static const std::string getTimingsTable(const Timer& timer);
 
+  /**
+   * \brief Generates a human readable tree structure with information about
+   * the registered timers.
+   * \param timer Timer to be transformed into a tree string.
+   * \return A string containing the generated tree.
+   */
   static const std::string getTimingsTree(const Timer& timer);
 
  private:
+
   static const std::string getSubTreeTimings(const Timer& timer,
                                              const std::string& parent_timer,
                                              const std::string& indentation);
 
+  /// \brief Color codes for terminal output.
   enum COLOR {
     RED = 31,
     GREEN = 32,
@@ -29,7 +42,11 @@ class TimerPrinter {
     DEF = 39
   };
 
+  /// \brief Generates a string which is printed as bold in the terminal.
   static const std::string boldify(const std::string& s);
+
+  /// \brief Generates a string which is printed in the color specified in
+  /// the second argument in the terminal.
   static const std::string color(const std::string& s, const COLOR color);
 
   static const std::string TIMER_INDENTATION;
